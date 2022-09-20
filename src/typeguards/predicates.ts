fc09dd997ac92f8ee29f70288b95c21c834f4c27
b3nThomas <b3nadthomas@gmail.com>
2022-09-20 22:33:09 +0100

    feat: extend predicate examples

--- src/typeguards/predicates.ts
+++ src/typeguards/predicates.ts
@@ -5,16 +5,42 @@
 
 type PowerType = 'Mechanical' | 'Manual';
 
-type Vehicle<
+type BaseVehicle<
     T extends {
         __powerType: PowerType;
     }
 > = T;
 
-type Car = Vehicle<{
-    __powerType: 'Mechanical';
+type ManualVehicle = BaseVehicle<{
+    __powerType: 'Manual';
+    powerAction: 'pedal' | 'row' | 'kick' | 'glide';
 }>;
 
-type Skateboard = Vehicle<{
-    __powerType: 'Manual';
+// const skateboard: ManualVehicle = {
+//     __powerType: 'Manual',
+//     powerAction: 'kick'
+// };
+
+type MechanicalVehical = BaseVehicle<{
+    __powerType: 'Mechanical';
+    engineType: 'petrol' | 'diesel' | 'hybrid' | 'electric';
 }>;
+
+// const ferrari: MechanicalVehical = {
+//     __powerType:'Mechanical',
+//     engineType: 'petrol'
+// };
+
+type Vehicle = MechanicalVehical | ManualVehicle;
+
+const vehicleHandler = (vehicle: Vehicle) => {
+    switch (vehicle.__powerType) {
+        case 'Manual':
+            console.log(`${vehicle.powerAction} to move this vehicle`);
+            break;
+        case 'Mechanical':
+            console.log(`This vehicles engine is ${vehicle.engineType}`);
+    }
+};
+
+// vehicleHandler(ferrari);
