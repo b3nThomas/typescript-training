24f4d1a6037f6de63fbdfd2589af112af1c97443
b3nThomas <b3nadthomas@gmail.com>
2022-10-15 09:26:40 +0100

    feat: map / mapErr

--- src/neverthrow/Result.map.ts
+++ src/neverthrow/Result.map.ts
@@ -1,8 +1,29 @@
 import { err, ok, Result } from 'neverthrow';
+import { setTimeout } from 'timers/promises';
 
-type CheeseError = { message: 'Found some cheese' };
+type DataResponse = { data: string };
+type ErrorResponse = { message: string };
 
-const errWhenCheeseFound = (text: string): Result<string, CheeseError> => {
-    throw new Error();
-    return text.includes('cheese') ? err({ message: 'Found some cheese' }) : ok(text);
+const callApi = async (): Promise<Result<DataResponse, ErrorResponse>> => {
+    await setTimeout(2e3);
+    const success = Math.round(Math.random());
+    if (success) {
+        return ok({ data: 'Some Data' });
+    } else {
+        return err({ message: 'Something went wrong' });
+    }
 };
+
+(async () => {
+    const data = await callApi();
+
+    // Only runs when Result was ok
+    data.map((data) => {
+        console.log(data.data.toUpperCase());
+    });
+
+    // Only runs when Result was err
+    data.mapErr((err) => {
+        console.error(err.message.toUpperCase());
+    });
+})();
